a1b4705bb5f1e965d8e0d1f5ea8a289949e72cbb
Dimas Anwar Aziz <dimasanwaraziz@gmail.com>
2024-12-17 14:48:49 +0700

    Encoding process

--- kode/main.go
+++ kode/main.go
@@ -10,65 +10,9 @@ import (
 	"os"
 )
 
-// Fungsi encode
-func encode(inputImage, outputImage, message string) {
-	img, err := loadImage(inputImage)
-	if err != nil {
-		fmt.Println("Error loading image:", err)
-		return
-	}
-	encodedImg := hideMessageInImage(img, message)
-	err = saveImage(encodedImg, outputImage)
-	if err != nil {
-		fmt.Println("Error saving image:", err)
-		return
-	}
-	fmt.Println("Pesan berhasil disisipkan ke gambar:", outputImage)
-}
-
-// Fungsi decode
-func decode(inputImage string) {
-	img, err := loadImage(inputImage)
-	if err != nil {
-		fmt.Println("Error loading image:", err)
-		return
-	}
-	message := extractMessageFromImage(img)
-	fmt.Println("Pesan yang disembunyikan:", message)
-}
-
-// Fungsi loadImage yang mendukung JPG dan PNG
-func loadImage(filename string) (image.Image, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// Decode gambar dengan format PNG atau JPG
-	img, format, err := image.Decode(file)
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println("Loaded image format:", format)
-	return img, nil
-}
-
-// Fungsi saveImage yang menyimpan hanya dalam format PNG
-func saveImage(img image.Image, filename string) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// Selalu menyimpan dalam format PNG
-	return png.Encode(file, img)
-}
-
-func hideMessageInImage(img image.Image, message string) image.Image {
-	message += "\000" // Tambahkan terminator
+// Fungsi utama untuk encoding pesan ke gambar
+func hideMessageInImage(img image.Image, message string) *image.RGBA {
+	message += "\000" // Menambahkan terminator
 	messageBits := []byte(message)
 	bitIndex := 0
 	newImg := image.NewRGBA(img.Bounds())
@@ -79,52 +23,43 @@ func hideMessageInImage(img image.Image, message string) image.Image {
 
 	for y := 0; y < img.Bounds().Dy(); y++ {
 		for x := 0; x < img.Bounds().Dx(); x++ {
-			r, g, b, a := img.At(x, y).RGBA()
+			if bitIndex >= len(messageBits)*8 {
+				newImg.Set(x, y, img.At(x, y))
+				continue
+			}
 
+			r, g, b, a := rgbaToUint8(img.At(x, y))
+
+			r = setLSB(r, extractBit(messageBits, bitIndex))
+			bitIndex++
 			if bitIndex < len(messageBits)*8 {
-				oldR := r
-				r = (r & 0xFFFE) | uint32((messageBits[bitIndex/8]>>(7-(bitIndex%8)))&1)
-				fmt.Printf("Pixel (%d, %d) - R: %d -> %d\n", x, y, oldR&1, r&1)
+				g = setLSB(g, extractBit(messageBits, bitIndex))
+				bitIndex++
+			}
+			if bitIndex < len(messageBits)*8 {
+				b = setLSB(b, extractBit(messageBits, bitIndex))
 				bitIndex++
 			}
 
-			if bitIndex < len(messageBits)*8 {
-				oldG := g
-				g = (g & 0xFFFE) | uint32((messageBits[bitIndex/8]>>(7-(bitIndex%8)))&1)
-				fmt.Printf("Pixel (%d, %d) - G: %d -> %d\n", x, y, oldG&1, g&1)
-				bitIndex++
-			}
-
-			if bitIndex < len(messageBits)*8 {
-				oldB := b
-				b = (b & 0xFFFE) | uint32((messageBits[bitIndex/8]>>(7-(bitIndex%8)))&1)
-				fmt.Printf("Pixel (%d, %d) - B: %d -> %d\n", x, y, oldB&1, b&1)
-				bitIndex++
-			}
-
-			newColor := color.RGBA{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)}
-			newImg.Set(x, y, newColor)
+			newImg.Set(x, y, color.RGBA{r, g, b, a})
 		}
 	}
-
 	fmt.Println("=== DEBUG ENCODING END ===")
 	return newImg
 }
 
+// Fungsi utama untuk decoding pesan dari gambar
 func extractMessageFromImage(img image.Image) string {
 	var messageBytes []byte
-	currentByte := byte(0)
+	var currentByte byte
 	bitIndex := 0
 
 	fmt.Println("=== DEBUG DECODING START ===")
-
 	for y := 0; y < img.Bounds().Dy(); y++ {
 		for x := 0; x < img.Bounds().Dx(); x++ {
-			r, g, b, _ := img.At(x, y).RGBA()
+			r, g, b, _ := rgbaToUint8(img.At(x, y))
 
-			// Ekstrak bit LSB dari R, G, B
-			currentByte = (currentByte << 1) | byte(r&1)
-			fmt.Printf("Pixel (%d, %d) - R: %d, CurrentByte: %08b\n", x, y, r&1, currentByte)
+			currentByte = (currentByte << 1) | (r & 1)
 			bitIndex++
 			if bitIndex == 8 {
 				if currentByte == 0 {
@@ -136,8 +71,7 @@ func extractMessageFromImage(img image.Image) string {
 				bitIndex = 0
 			}
 
-			currentByte = (currentByte << 1) | byte(g&1)
-			fmt.Printf("Pixel (%d, %d) - G: %d, CurrentByte: %08b\n", x, y, g&1, currentByte)
+			currentByte = (currentByte << 1) | (g & 1)
 			bitIndex++
 			if bitIndex == 8 {
 				if currentByte == 0 {
@@ -149,8 +83,7 @@ func extractMessageFromImage(img image.Image) string {
 				bitIndex = 0
 			}
 
-			currentByte = (currentByte << 1) | byte(b&1)
-			fmt.Printf("Pixel (%d, %d) - B: %d, CurrentByte: %08b\n", x, y, b&1, currentByte)
+			currentByte = (currentByte << 1) | (b & 1)
 			bitIndex++
 			if bitIndex == 8 {
 				if currentByte == 0 {
@@ -163,9 +96,50 @@ func extractMessageFromImage(img image.Image) string {
 			}
 		}
 	}
-
 	fmt.Println("=== DEBUG DECODING END ===")
 	return string(messageBytes)
+}
+
+// Helper untuk ekstrak bit dari array byte
+func extractBit(data []byte, index int) uint8 {
+	return (data[index/8] >> (7 - uint(index%8))) & 1
+}
+
+// Helper untuk mengatur LSB pada byte
+func setLSB(value, bit uint8) uint8 {
+	return (value & 0xFE) | bit
+}
+
+// Helper untuk mengubah nilai RGBA menjadi uint8
+func rgbaToUint8(c color.Color) (r, g, b, a uint8) {
+	r32, g32, b32, a32 := c.RGBA()
+	return uint8(r32 >> 8), uint8(g32 >> 8), uint8(b32 >> 8), uint8(a32 >> 8)
+}
+
+// Fungsi load gambar
+func loadImage(filename string) (image.Image, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	img, format, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Loaded image format:", format)
+	return img, nil
+}
+
+// Fungsi save gambar
+func saveImage(img *image.RGBA, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return png.Encode(file, img)
 }
 
 func main() {
@@ -180,13 +154,29 @@ func main() {
 			fmt.Println("Usage: -mode encode -input <input.png> -output <output.png> -message <message>")
 			return
 		}
-		encode(*inputImage, *outputImage, *message)
+		img, err := loadImage(*inputImage)
+		if err != nil {
+			fmt.Println("Error loading image:", err)
+			return
+		}
+		encodedImg := hideMessageInImage(img, *message)
+		if err := saveImage(encodedImg, *outputImage); err != nil {
+			fmt.Println("Error saving image:", err)
+			return
+		}
+		fmt.Println("Pesan berhasil disisipkan ke gambar:", *outputImage)
 	} else if *mode == "decode" {
 		if *inputImage == "" {
 			fmt.Println("Usage: -mode decode -input <encoded.png>")
 			return
 		}
-		decode(*inputImage)
+		img, err := loadImage(*inputImage)
+		if err != nil {
+			fmt.Println("Error loading image:", err)
+			return
+		}
+		message := extractMessageFromImage(img)
+		fmt.Println("Pesan yang disembunyikan:", message)
 	} else {
 		fmt.Println("Mode tidak dikenal. Gunakan 'encode' atau 'decode'.")
 	}
